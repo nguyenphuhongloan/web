6fdc46023a4fc9a77fdb6abca00ee9cd71906fef
nguyenphuhongloan <nguyenphuhongloan@gmail.com>
2021-07-21 02:42:38 +0700

    sua lai

--- modules/admin/modulesad/giaodienad/themcv.php
+++ modules/admin/modulesad/giaodienad/themcv.php
@@ -1,26 +1,29 @@
 <div class="right">
-                    <div class="right__content">
-                        <div class="right__title">Bảng điều khiển</div>
-                        <p class="right__desc">Thêm chức vụ</p>
-                        <div class="right__formWrapper">
-                            <div style="max-width: 400px;margin:0 auto;">
-                                <div class="right__inputWrapper">
-                                    <label for="title">Chức vụ</label>
-                                    <input type="text" placeholder="chức vụ" id="tencv">
-                                </div>
-                                <button class="btn" type="submit" onclick="themcv()">Thêm</button>
-                            </div>
-                        </div>
-                    </div>
+    <div class="right__content">
+        <div class="right__title">Bảng điều khiển</div>
+        <p class="right__desc">Thêm chức vụ</p>
+        <div class="right__formWrapper">
+            <div style="max-width: 400px;margin:0 auto;">
+                <div class="right__inputWrapper">
+                    <label for="title">Chức vụ</label>
+                    <input type="text" placeholder="chức vụ" id="tencv">
                 </div>
+                <button class="btn" type="submit" onclick="themcv()">Thêm</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
-    function themcv(){
-        var cv=$('#tencv').val();
-        $.post("modulesad/xuly.php",{cv:cv,action:"themcv"},function(result){
-            if(result==1){
+    function themcv() {
+        var cv = $('#tencv').val();
+        $.post("modulesad/xuly.php", {
+            cv: cv,
+            action: "themcv"
+        }, function(result) {
+            if (result == 1) {
                 alert("Thêm thành công");
-            }
-            else
+            } else
                 alert("Thêm thất bại");
         });
     }
